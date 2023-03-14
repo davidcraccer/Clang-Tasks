b563fad71018391716356fee8c96e32ecc316281
davidcraccer <127027894+davidcraccer@users.noreply.github.com>
2023-03-14 01:24:14 +0100

    Add files via upload

---
+++ InsertionSort/introprog_insertionsort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arrayio.h"
+
+int MAX_LAENGE = 1000;
+
+void insertion_sort(int array[], int len) {
+    /*
+     * Hier Insertionsort implementieren!
+     * Diese Funktion soll das Eingabearray nach dem
+     * Insertionsort-Verfahren sortieren.
+     * Hinweis: Verwende die "in place"-Variante! D.h. der
+     * Sortiervorgang soll auf dem originalen Array stattfinden und
+     * kein zweites verwendet werden.
+     */
+    int i, j, key;  
+    for (i = 1; i < len; i++) {  
+        key = array[i];  
+        j = i - 1;  
+  
+        while(j>=0 && key <= array[j]){    
+            array[j+1] = array[j];     
+            j = j-1;    
+        }    
+        array[j+1] = key;    
+    }  
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2){
+        printf("Aufruf: %s <Dateiname>\n", argv[0]);
+        printf("Beispiel: %s zahlen.txt\n", argv[0]);
+        exit(1);
+    }
+
+    char *filename = argv[1];
+
+    int array[MAX_LAENGE];
+    int len = read_array_from_file(array, MAX_LAENGE, filename);
+
+    printf("Unsortiertes Array:");
+    print_array(array, len);
+
+    /* Aufruf Insertionsort */
+    insertion_sort(array, len);
+
+    printf("Sortiertes Array:");
+    print_array(array, len);
+
+    return 0;
+}
